7557752e50090c2c655a2d6371ecb2f002172c8b
deborahalynne <62914300+deborahalynne@users.noreply.github.com>
2021-09-28 22:36:47 -0300

    Sync
    Comite para adicionar Front End ao projeto da Livraria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ListComponent } from 'src/views/list/list.component';
+import { CadastroComponent } from 'src/views/cadastro/cadastro.component';
+import { HomeComponent } from 'src/views/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule} from '@angular/material/table';
+import { MatOptionModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    HomeComponent,
+    CadastroComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatOptionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
